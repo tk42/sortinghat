76e96dc19bc1cba7f95f8bf486cc56d6f73f1dbc
tk42 <nsplat@gmail.com>
2024-07-11 06:39:21 +0000

    chore(api): Update dependencies, add bcrypt for password hashing, and implement user sign-in and password update endpoints

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,10 +1,16 @@
+import json
+
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
 from .graphql import client, gql
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 router = APIRouter(
@@ -21,14 +27,31 @@ class User(BaseModel):
     given_name: Optional[str] = None
     school_id: Optional[int] = None
     email: str
+    password: Optional[str] = None
     status: Optional[int] = None
     created_at: Optional[datetime] = None
     updated_at: Optional[datetime] = None
     expired_at: Optional[datetime] = None
 
 
+class SignInEndpoint(BaseModel):
+    email: str
+    password: str
+
+
+class UpdatePasswordEndpoint(BaseModel):
+    user_id: int
+    old_password: str
+    new_password: str
+
+
+def verify(password: str, hashed: str):
+    salt = hashed[7:29]
+    return hashed == pwd_context.hash(password, salt=salt)
+
+
 @router.post("/signin", response_class=JSONResponse)
-async def signin(email: str) -> Optional[User]:
+async def signin(item: SignInEndpoint) -> Optional[User]:
     """
     :return: User
     """
@@ -44,6 +67,7 @@ async def signin(email: str) -> Optional[User]:
               given_name
               school_id
               email
+              password
               status
               created_at
               updated_at
@@ -56,18 +80,111 @@ async def signin(email: str) -> Optional[User]:
     # Execute the query on the transport
     result = await client.execute_async(
         query,
-        variable_values={"email": email}
+        variable_values={"email": item.email}
     )
 
     users = result["teachers"]
 
-    if len(users) == 0:
-        return None
+    if len(users) != 1:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="No user by the email"
+        )
 
-    return users[0]
+    user = users[0]
+
+    if not verify(item.password, user["password"]):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Unauthorized"
+        )
+
+    return User(**{k: v for k, v in user.items() if k != 'password'})
 
 
-@router.patch("/update", response_class=JSONResponse)
+@router.post("/password", response_class=JSONResponse)
+async def update_password(item: UpdatePasswordEndpoint) -> bool:
+    """
+    :return: User
+    """
+
+    query = gql(
+        """
+        query getPassword($user_id: bigint!) {
+            teachers_by_pk(id: $user_id) {
+              id
+              password
+            }
+        }
+        """
+    )
+
+    # Execute the query on the transport
+    result = await client.execute_async(
+        query,
+        variable_values={"user_id": item.user_id}
+    )
+
+    user = result["teachers_by_pk"]
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="No user by the email"
+        )
+
+    if not verify(item.old_password, user["password"]):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Unauthorized by the old password"
+        )
+
+    encrypted_password = pwd_context.hash(item.new_password)
+
+    query = gql(
+        """
+        mutation updateTeacher(
+          $user_id: bigint!,
+          $encrypted_password: String,
+          $updated_at: timestamp,
+        ) {
+            update_teachers_by_pk(
+              pk_columns: {id: $user_id}, 
+              _set: {
+                password: $encrypted_password,
+                updated_at: $updated_at
+              }
+            ) {
+              id
+              password
+              updated_at
+            }
+        }
+        """
+    )
+
+    # Execute the query on the transport
+    result = await client.execute_async(
+        query,
+        variable_values={
+            "user_id": item.user_id,
+            "encrypted_password": encrypted_password,
+            "updated_at": datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f') + 'Z'
+        }
+    )
+
+    try:
+        updated_user = result["update_teachers_by_pk"]
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Unauthorized by unknown reason"
+        )
+
+    return True
+
+
+@router.patch("/user", response_class=JSONResponse)
 async def update(user: User) -> User:
     """
     :return: User
@@ -130,7 +247,7 @@ async def update(user: User) -> User:
     return updated_user
 
 
-@router.delete("/delete", response_class=JSONResponse)
+@router.delete("/user", response_class=JSONResponse)
 async def delete(user_id: int) -> bool:
     """
     :return: bool
