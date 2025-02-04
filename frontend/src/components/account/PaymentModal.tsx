e8bf4dad647a5bef57f174580ea54d3443bb7f78
tk42 <nsplat@gmail.com>
2025-02-04 09:42:20 +0900

    refactor: Update Stripe API keys to use template literals for dynamic values

--- frontend/src/components/account/PaymentModal.tsx
+++ frontend/src/components/account/PaymentModal.tsx
@@ -2,7 +2,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { CheckoutForm } from './CheckoutForm';
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!}`);
 
 interface PaymentModalProps {
     isOpen: boolean;
